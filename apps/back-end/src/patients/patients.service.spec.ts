b7467bc33ea6598789b66832124225f0c512aeaf
maikofelix47 <maikofelix89@gmail.com>
2023-05-30 20:28:06 +0300

    BE: Fixed failing patients tests

--- apps/back-end/src/patients/patients.service.spec.ts
+++ apps/back-end/src/patients/patients.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientsService } from './patients.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('PatientsService', () => {
   let service: PatientsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientsService],
+      providers: [
+        PatientsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<PatientsService>(PatientsService);
+    prismaService = module.get<PrismaService>(PatientsService);
   });
 
-  it('should be defined', () => {
+  it('patients service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
