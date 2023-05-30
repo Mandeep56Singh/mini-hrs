3626b22e142f0fa20919e7233c3adb8d49d3b730
maikofelix47 <maikofelix89@gmail.com>
2023-05-30 20:41:28 +0300

    BE: Fixed visits service tests

--- apps/back-end/src/visits/visits.service.spec.ts
+++ apps/back-end/src/visits/visits.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VisitsService } from './visits.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('VisitsService', () => {
   let service: VisitsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VisitsService],
+      providers: [
+        VisitsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<VisitsService>(VisitsService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('visits service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
