2f0547fda379123a37cb8c254a7547232465f532
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 12:54:25 +0300

    BE: Fixed users service test

--- apps/back-end/src/users/users.service.spec.ts
+++ apps/back-end/src/users/users.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('users service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
