4835af2b2b6b7a005813534f27a7351196b5242a
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 13:24:51 +0300

    BE: Fixed app controller tests

--- apps/back-end/src/app/app.controller.spec.ts
+++ apps/back-end/src/app/app.controller.spec.ts
@@ -3,20 +3,48 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mockAppService = {
+  getData: jest.fn(() => {
+    return { message: 'Hello API' };
+  }),
+};
+
 describe('AppController', () => {
-  let app: TestingModule;
+  let appModule: TestingModule;
+  let appService: AppService;
+  let appController: AppController;
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
+    appModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockAppService,
+        },
+      ],
     }).compile();
+
+    appController = appModule.get<AppController>(AppController);
+    appService = appModule.get<AppService>(AppService);
+  });
+
+  describe('app controller and dependencies defined', () => {
+    it('app controller should be defined', () => {
+      expect(appController).toBeDefined();
+    });
+
+    it('app service should be defined', () => {
+      expect(appService).toBeDefined();
+    });
   });
 
   describe('getData', () => {
     it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      jest.spyOn(appService, 'getData');
+      const receivedMsg = appController.getData();
+      expect(appService.getData).toHaveBeenCalled();
+      expect(receivedMsg).toEqual({ message: 'Hello API' });
     });
   });
 });
