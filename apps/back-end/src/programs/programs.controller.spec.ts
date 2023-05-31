9f87d6b14d58e14494315b6d517a51b3b3906892
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 13:36:36 +0300

    BE: Fixed programs controller test

--- apps/back-end/src/programs/programs.controller.spec.ts
+++ apps/back-end/src/programs/programs.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProgramsController } from './programs.controller';
+import { ProgramsService } from './programs.service';
+
+const mockProgramsService = {};
 
 describe('ProgramsController', () => {
   let controller: ProgramsController;
+  let programService: ProgramsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: ProgramsService,
+          useValue: mockProgramsService,
+        },
+      ],
       controllers: [ProgramsController],
     }).compile();
 
     controller = module.get<ProgramsController>(ProgramsController);
+    programService = module.get<ProgramsService>(ProgramsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('programs controller and its dependancies should be defined', () => {
+    it('programs controller should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('programs service should be defined', () => {
+      expect(programService).toBeDefined();
+    });
   });
 });
