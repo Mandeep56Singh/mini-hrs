95e5202428de8bbf36dbb1aea1cbbe9bec621408
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 13:57:20 +0300

    BE: program enrollment controller and its dependancies should be defined

--- apps/back-end/src/program-enrollments/program-enrollments.controller.spec.ts
+++ apps/back-end/src/program-enrollments/program-enrollments.controller.spec.ts
@@ -1,20 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProgramEnrollmentsController } from './program-enrollments.controller';
+import { PatientsService } from '../patients/patients.service';
+import { ProgramsService } from '../programs/programs.service';
+import { LocationsService } from '../locations/locations.service';
+import { ProgramEnrollmentsService } from './program-enrollments.service';
+
+const mockPatientsService = {
+
+};
+const mockProgramsService = {
+
+};
+const mockLocationsService = {
+
+};
+const mockProgramEnrollmentsService = {
+
+};
 
 describe('ProgramEnrollmentsController', () => {
   let controller: ProgramEnrollmentsController;
+  let patientsService: PatientsService;
+  let programsService: ProgramsService;
+  let locationsService: LocationsService;
+  let programEnrollmentsService: ProgramEnrollmentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: PatientsService,
+          useValue: mockPatientsService
+        },
+        {
+          provide: ProgramsService,
+          useValue: mockProgramsService
+        },
+        {
+          provide: LocationsService,
+          useValue: mockLocationsService
+        },
+        {
+          provide: ProgramEnrollmentsService,
+          useValue: mockProgramEnrollmentsService
+        }
+      ],
       controllers: [ProgramEnrollmentsController],
     }).compile();
 
     controller = module.get<ProgramEnrollmentsController>(
       ProgramEnrollmentsController
     );
+    patientsService = module.get<PatientsService>(PatientsService);
+    programsService = module.get<ProgramsService>(ProgramsService);
+    locationsService = module.get<LocationsService>(LocationsService);
+    programEnrollmentsService = module.get<ProgramEnrollmentsService>(ProgramEnrollmentsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('program enrollment controller and its dependancies should be defined',()=>{
+
+    it('program enrollments controller should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+    it('patients service should be defined', () => {
+      expect(patientsService).toBeDefined();
+    });
+    it('programs service should be defined', () => {
+      expect(programsService).toBeDefined();
+    });
+    it('locations service should be defined', () => {
+      expect(locationsService).toBeDefined();
+    });
+    it('program enrollments service should be defined', () => {
+      expect(programEnrollmentsService).toBeDefined();
+    });
+
   });
 });
