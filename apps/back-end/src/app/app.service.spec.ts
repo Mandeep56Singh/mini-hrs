a1d5abe756bd0e41df3d23d06617537824bc6c92
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 12:37:07 +0300

    BE: Fixed app service test

--- apps/back-end/src/app/app.service.spec.ts
+++ apps/back-end/src/app/app.service.spec.ts
@@ -1,21 +1,38 @@
 import { Test } from '@nestjs/testing';
 
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
+
 
 describe('AppService', () => {
   let service: AppService;
+  let prismaService: PrismaService;
 
   beforeAll(async () => {
-    const app = await Test.createTestingModule({
-      providers: [AppService],
+    const appModule = await Test.createTestingModule({
+      providers: [
+        AppService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
-    service = app.get<AppService>(AppService);
+    service = appModule.get<AppService>(AppService);
+    prismaService = appModule.get<PrismaService>(PrismaService);
   });
 
   describe('getData', () => {
     it('should return "Hello API"', () => {
       expect(service.getData()).toEqual({ message: 'Hello API' });
+    });
+    it('prisma service should be defined"', () => {
+      expect(prismaService).toBeDefined();
     });
   });
 });
