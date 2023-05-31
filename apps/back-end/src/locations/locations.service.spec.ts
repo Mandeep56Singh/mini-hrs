f222de997123435aed2cd73a5cba9db1f1082965
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 11:22:59 +0300

    BE: Fixed location service test

--- apps/back-end/src/locations/locations.service.spec.ts
+++ apps/back-end/src/locations/locations.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('LocationsService', () => {
   let service: LocationsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+      providers: [
+        LocationsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('location service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
