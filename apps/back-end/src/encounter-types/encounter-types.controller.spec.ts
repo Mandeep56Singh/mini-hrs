246ef2cc48e56a1fefc6b7db1b74f78295b7c2dc
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 11:46:58 +0300

    BE: Fixed encounter types controller and service test

--- apps/back-end/src/encounter-types/encounter-types.controller.spec.ts
+++ apps/back-end/src/encounter-types/encounter-types.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EncounterTypesController } from './encounter-types.controller';
+import { VisitTypesService } from '../visit-types/visit-types.service';
+import { EncounterTypesService } from './encounter-types.service';
+
+const mockEncounterTypeService = {
+
+};
+
+const mockVisitTypesService = {
+
+};
 
 describe('EncounterTypesController', () => {
   let controller: EncounterTypesController;
+  let encounterTypesService: EncounterTypesService;
+  let visitTypesService: VisitTypesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers:[
+        {
+          provide: EncounterTypesService,
+          useValue: mockEncounterTypeService
+        },
+        {
+          provide: VisitTypesService,
+          useValue: mockVisitTypesService
+        }
+      ],
       controllers: [EncounterTypesController],
     }).compile();
 
     controller = module.get<EncounterTypesController>(EncounterTypesController);
+    encounterTypesService = module.get<EncounterTypesService>(EncounterTypesService);
+    visitTypesService = module.get<VisitTypesService>(VisitTypesService);
   });
 
-  it('should be defined', () => {
+  it('encounter-types controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('encounter-types service should be defined', () => {
+    expect(encounterTypesService).toBeDefined();
+  });
+  it('visit-types service should be defined', () => {
+    expect(visitTypesService).toBeDefined();
   });
 });
--- apps/back-end/src/encounter-types/encounter-types.service.spec.ts
+++ apps/back-end/src/encounter-types/encounter-types.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EncounterTypesService } from './encounter-types.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('EncounterTypesService', () => {
   let service: EncounterTypesService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EncounterTypesService],
+      providers: [
+        EncounterTypesService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<EncounterTypesService>(EncounterTypesService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('encounter-types service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
