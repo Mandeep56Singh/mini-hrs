7b39158c637d95aedd29494d393277e506f897fd
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 11:28:00 +0300

    BE: Fixed patients controller test

--- apps/back-end/src/patients/patients.controller.spec.ts
+++ apps/back-end/src/patients/patients.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientsController } from './patients.controller';
+import { PatientsService } from './patients.service';
 
+const mockPatientsService = {
+
+};
 describe('PatientsController', () => {
   let controller: PatientsController;
+  let patientsService: PatientsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PatientsController],
+      providers:[
+        {
+          provide: PatientsService,
+          useValue: mockPatientsService
+        }
+      ],
+      controllers: [
+        PatientsController
+      ],
     }).compile();
 
     controller = module.get<PatientsController>(PatientsController);
+    patientsService = module.get<PatientsService>(PatientsService);
   });
 
-  it('should be defined', () => {
+  it('patients controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('patients service should be defined', () => {
+    expect(patientsService).toBeDefined();
   });
 });
