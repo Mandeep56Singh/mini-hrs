3d2789a5e0aceefe413758b9f07623cf60e62c8c
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 11:52:39 +0300

    BE:Fixed visittypes controller test

--- apps/back-end/src/visit-types/visit-types.controller.spec.ts
+++ apps/back-end/src/visit-types/visit-types.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VisitTypesController } from './visit-types.controller';
+import { VisitTypesService } from './visit-types.service';
+import { ProgramsService } from '../programs/programs.service';
+
+const mockVisitTypesService = {
+
+};
+
+const mockProgramsService = {
+
+};
 
 describe('VisitTypesController', () => {
   let controller: VisitTypesController;
+  let visitTypeService: VisitTypesService;
+  let programService: ProgramsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers:[
+        {
+          provide: VisitTypesService,
+          useValue: mockVisitTypesService
+        },
+        {
+          provide: ProgramsService,
+          useValue: mockProgramsService
+        }
+      ],
       controllers: [VisitTypesController],
     }).compile();
 
     controller = module.get<VisitTypesController>(VisitTypesController);
+    visitTypeService = module.get<VisitTypesService>(VisitTypesService);
+    programService = module.get<ProgramsService>(ProgramsService);
   });
 
-  it('should be defined', () => {
+  it('visit-types controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('visit-types service should be defined', () => {
+    expect(visitTypeService).toBeDefined();
+  });
+  it('programs service should be defined', () => {
+    expect(programService).toBeDefined();
   });
 });
