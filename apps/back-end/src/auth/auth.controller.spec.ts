270633efb4d09f8b189126d055da6d8af34327ef
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 14:03:31 +0300

    BE: auth controller and its dependancies should be defined

--- apps/back-end/src/auth/auth.controller.spec.ts
+++ apps/back-end/src/auth/auth.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+const mockAuthService = {
+
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService
+        }
+      ],
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('auth controller and its dependancies should be defined',()=>{
+
+    it('auth controller should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('auth service should be defined', () => {
+      expect(authService).toBeDefined();
+    });
+
   });
+
 });
