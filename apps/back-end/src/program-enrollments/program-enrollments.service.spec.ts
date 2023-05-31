ccc826b59c8bb32daa9ac2effd93f5c0073c2d10
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 12:57:14 +0300

    BE: Fixed program enrollments service test

--- apps/back-end/src/program-enrollments/program-enrollments.service.spec.ts
+++ apps/back-end/src/program-enrollments/program-enrollments.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProgramEnrollmentsService } from './program-enrollments.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('ProgramEnrollmentsService', () => {
   let service: ProgramEnrollmentsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProgramEnrollmentsService],
+      providers: [
+        ProgramEnrollmentsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<ProgramEnrollmentsService>(ProgramEnrollmentsService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('program-enrollments service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
