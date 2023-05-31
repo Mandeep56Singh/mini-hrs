d4f4abee070ee41fe32439ce366e40bc64a6ba21
maikofelix47 <maikofelix89@gmail.com>
2023-05-31 11:12:16 +0300

    BE: Fixed program service tests

--- apps/back-end/src/programs/programs.service.spec.ts
+++ apps/back-end/src/programs/programs.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProgramsService } from './programs.service';
+import { PrismaService } from '../app/prisma/prisma.service';
+
+const mockPrismaService = {
+
+};
 
 describe('ProgramsService', () => {
   let service: ProgramsService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProgramsService],
+      providers: [
+        ProgramsService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
     service = module.get<ProgramsService>(ProgramsService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('program service should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('prisma service should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
